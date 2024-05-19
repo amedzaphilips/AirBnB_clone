80b8e4ae05637ba92ba92b87c8eda0ff0b59d7f3
Joshua <atsujoshua67@gmail.com>
2024-05-19 15:24:23 -0700

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -21,3 +21,16 @@ class BaseModel:
         [<class name>] (<self.id>) <self.__dict__>"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at
+        with the current datetime"""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """return a dictionary containing __class__"""
+        my_dict = self.__dict__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
