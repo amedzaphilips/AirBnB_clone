91b81d4a2b0fedb6b9d3e0f42c5f7a90d7b24973
Joshua <atsujoshua67@gmail.com>
2024-05-19 16:35:51 -0700

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
 
     def __init__(self):
         """Instantiatiing with id
-        Args: 
+        Args:
             string id - assign with an uuid when an instance is created"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
