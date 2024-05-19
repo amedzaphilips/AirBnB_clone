1c6b65b7cade85d3c4c87206c2c50da16eeb395c
Joshua <atsujoshua67@gmail.com>
2024-05-19 16:00:36 -0700

    AirBnB

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,21 @@
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test all cases"""
+
+    def test_is_class(self):
+        base = BaseModel()
+        self.assertIsInstance(base, BaseModel)
+
+    def test_id(self):
+        base = BaseModel()
+        base1 = BaseModel()
+        self.assertNotEqual(base.id, base1.id)
+
+    def test_update(self):
+        base = BaseModel()
+        base.save()
+        self.assertNotEqual(base.created_at, base.updated_at)
+
